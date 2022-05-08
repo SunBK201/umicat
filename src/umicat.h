0a6fb907e984d2d90516f740d1846b372e7ae7e6
SunBK201 <sunbk201gm@gmail.com>
2022-05-08 13:35:41 +0800

    feat: release 0.3.0

--- src/umicat.h
+++ src/umicat.h
@@ -5,8 +5,8 @@
 #ifndef _UMICAT_H_INCLUDED_
 #define _UMICAT_H_INCLUDED_
 
-#define umicat_version 0000203
-#define UMICAT_VERSION "0.2.3"
+#define umicat_version 0000300
+#define UMICAT_VERSION "0.3.0"
 #define UMICAT_VER     "umicat/" UMICAT_VERSION
 
 #define UCT_CONF_PATH  "/etc/umicat/umicat.conf"
