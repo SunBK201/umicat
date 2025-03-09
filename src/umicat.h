e580a5eb5f1fbd61f5dda13313132058f3520ce0
SunBK201 <sunbk201gm@gmail.com>
2025-03-10 03:32:02 +0800

    docs: bump to 0.4.0

--- src/umicat.h
+++ src/umicat.h
@@ -5,8 +5,8 @@
 #ifndef _UMICAT_H_INCLUDED_
 #define _UMICAT_H_INCLUDED_
 
-#define umicat_version 0000303
-#define UMICAT_VERSION "0.3.3"
+#define umicat_version 0000400
+#define UMICAT_VERSION "0.4.0"
 #define UMICAT_VER     "umicat/" UMICAT_VERSION
 
 #define UCT_CONF_PATH  "/etc/umicat/umicat.conf"
